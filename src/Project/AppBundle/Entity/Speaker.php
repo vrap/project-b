39c3408c0caec3768796967cd18cba5961c0d42c
Pierre <pflauder@gmail.com>
2014-02-01 22:50:58 +0100

    Speaker Entity

---
+++ src/Project/AppBundle/Entity/Speaker.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Project\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Speaker
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Project\AppBundle\Entity\SpeakerRepository")
+ */
+class Speaker
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Project\AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="id", referencedColumnName="id")
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Project\AppBundle\Entity\User $user
+     * @return Speaker
+     */
+    public function setUser(\Project\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Project\AppBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
