3cf5c199ad049cbc4eadf71cbd1a341c85a827ac
flavien <flavien@flavien-VirtualBox.localdomain>
2014-02-03 22:52:04 +0100

    Add create database to sql file

--- project-b.sql
+++ project-b.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 -- Database: `project-b`
 --
 
+CREATE DATABASE IF NOT EXISTS `project-b`;
+use `project-b`;
+
 -- --------------------------------------------------------
 
 --
