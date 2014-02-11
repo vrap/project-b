563112b00067ed135bd4484d61a9d46dbfc1796d
vdaguenet <valentin.daguenet@gmail.com>
2014-02-11 11:44:15 +0100

    Table student_has_evaluation

---
+++ src/Project/AppBundle/Entity/StudentEvaluation.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Project\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * StudentEvaluation
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Project\AppBundle\Entity\StudentEvaluationRepository")
+ */
+class StudentEvaluation
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="score", type="float")
+     */
+    private $score;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="text")
+     */
+    private $comment;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Project\AppBundle\Entity\Student", cascade={"persist"})
+     */
+    private $student;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Project\AppBundle\Entity\Evaluation", cascade={"persist"})
+     */
+    private $evaluation;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set score
+     *
+     * @param float $score
+     * @return StudentEvaluation
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    /**
+     * Get score
+     *
+     * @return float 
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return StudentEvaluation
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string 
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set student
+     *
+     * @param \Project\AppBundle\Entity\Student $student
+     * @return StudentEvaluation
+     */
+    public function setStudent($student)
+    {
+        $this->student = $student;
+
+        return $this;
+    }
+
+    /**
+     * Get student
+     *
+     * @return \Project\AppBundle\Entity\Student
+     */
+    public function getStudent()
+    {
+        return $this->student;
+    }
+
+    /**
+     * Set evaluation
+     *
+     * @param \Project\AppBundle\Entity\Evaluation $evaluation
+     * @return StudentEvaluation
+     */
+    public function setEvaluation($evaluation)
+    {
+        $this->evaluation = $evaluation;
+
+        return $this;
+    }
+
+    /**
+     * Get evaluation
+     *
+     * @return \Project\AppBundle\Entity\Evaluation
+     */
+    public function getEvaluation()
+    {
+        return $this->evaluation;
+    }
+}
