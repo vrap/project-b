fd1fb170fb0e8c02e023b70d60346daaa6d09642
Flavien <Debflav@hotmail.com>
2014-02-14 14:37:07 +0100

    Override registration page

---
+++ src/Project/AppBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Project\AppBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder
+            ->add('username', null, array(/*'label' => 'form.username',*/ 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', 'email', array(/*'label' => 'form.email',*/ 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Password confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+        ;
+    }
+
+    public function getName()
+    {
+        return 'override_fosuser_registration';
+    }
+
+}
