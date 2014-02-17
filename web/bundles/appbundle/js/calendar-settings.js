defd9aa10b0497b703fcfd66a8b48e3f5b426df5
flavien <debar.flavien@gmail.com>
2014-02-17 22:30:21 +0100

    Update calendar (add delete dialog)

--- web/bundles/appbundle/js/calendar-settings.js
+++ web/bundles/appbundle/js/calendar-settings.js
@@ -4,10 +4,9 @@ $(function () {
     var m = date.getMonth();
     var y = date.getFullYear();*/
 
-     var calendar = $('#calendar-holder').fullCalendar({
+    $('#calendar-holder').fullCalendar({
         defaultView: 'month',
         selectable: true,
-        selectHelper: true,
         lazyFetching: true,
         header: {
             left: 'prev, next',
@@ -15,25 +14,50 @@ $(function () {
             right: 'month,basicWeek,basicDay,'
         },
         select: function(startDate, endDate, allDay) {
-            $( "#dialog" ).dialog({
+            $( "#dialog-create-events" ).dialog({
                 buttons: {
-                    Create: function() {
+                    Ajouter: function() {
                         startDate = startDate.getUTCFullYear() + "-" + (1 + startDate.getUTCMonth()) + "-" +  (1 + startDate.getUTCDate()) + " " + $("#start_time").val();
                         endDate = endDate.getUTCFullYear() + "-" + (1 + endDate.getUTCMonth()) + "-" +  (1 + endDate.getUTCDate()) + " " + $("#end_time").val();
                         var json = { name: $("#lesson_name").val(), startDate: startDate, endDate: endDate};
                         $.ajax({
-                            url     : Routing.generate('speaker_add_lesson', { data: JSON.stringify(json) } ), 
+                            url     : Routing.generate('agenda_add_lesson', { data: JSON.stringify(json) } ), 
                             type    : 'POST',
                             success : function(response){
-                                if(response == true) {
+                                if(response === true) {
                                     window.location.reload();
                                 }
                             }
                          });
+                    },
+                    Annuler: function() {
+                        $( this ).dialog( "close" );
                     }
                 }
             });
             return;
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+            var json = { name:calEvent.title, startDate:calEvent.start, endDate:calEvent.end };
+            $("<div>Voulez-vous vraiment supprimer cette entrée ?</div>").dialog({
+                buttons: {
+                  Delete: function() {
+                    $.ajax({
+                          url     : Routing.generate('agenda_delete_lesson', { id:calEvent.id } ), 
+                          type    : 'POST',
+                          success : function(response){
+                              if(response === true) {
+                                  window.location.reload();
+                              }
+                          }
+                      });
+                    },
+                  Cancel: function() {
+                    $( this ).dialog( "close" );
+                  }
+                }
+            });
+
         },
         timeFormat: {
             // for agendaWeek and agendaDay
