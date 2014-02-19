e2bda04d1f08f2a0c0a5e9389d9986c1ed1e0404
Romain <r.esilva@sysdream.com>
2014-02-19 21:18:39 +0100

    Add RedirectResponse to promotion controller

--- src/Project/AppBundle/Controller/PromotionController.php
+++ src/Project/AppBundle/Controller/PromotionController.php
@@ -3,6 +3,7 @@
 namespace Project\AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
