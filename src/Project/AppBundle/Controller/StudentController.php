33d0bb3e358d1881ae78d0d613e9c50f40135899
vdaguenet <valentin.daguenet@gmail.com>
2014-02-25 19:10:33 +0100

    Add a usable class

--- src/Project/AppBundle/Controller/StudentController.php
+++ src/Project/AppBundle/Controller/StudentController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 use Project\AppBundle\Entity\Student;
 use Project\AppBundle\Form\StudentType;
 
