07884c54f9255dd9efb3741db9aa4785a1b1958d
vdaguenet <valentin.daguenet@gmail.com>
2014-02-27 16:06:53 +0100

    Create archive_bilan entity

---
+++ src/Project/AppBundle/Entity/ArchiveBilan.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Project\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ArchiveBilan
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Project\AppBundle\Entity\ArchiveBilanRepository")
+ */
+class ArchiveBilan
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=45)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="blob")
+     */
+    private $content;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Project\AppBundle\Entity\Promotion", cascade={"persist"})
+     */
+    private $promotion;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return ArchiveBilan
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return ArchiveBilan
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set promotion
+     *
+     * @param \Project\AppBundle\Entity\Promotion $promotion
+     * @return ArchiveBilan
+     */
+    public function setPromotion(\Project\AppBundle\Entity\Promotion $promotion = null)
+    {
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+
+    /**
+     * Get promotion
+     *
+     * @return \Project\AppBundle\Entity\Promotion 
+     */
+    public function getPromotion()
+    {
+        return $this->promotion;
+    }
+}
